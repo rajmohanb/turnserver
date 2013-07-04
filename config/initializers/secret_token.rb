c29636b8474f6439480204a1846a2c5567ba95d0
Rajmohan <rajmohan@mindbricks.com>
2013-07-04 21:39:50 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Turnserver::Application.config.secret_token = 'ef24d635ba68a45fa821de0aeb6e0be72f6faff93c56b6c411fc26d522acfb823eebdc096f99ffe75d4bdac31e4d617ad6c1b5f0d8c405db6d0c00633ac5f043'
