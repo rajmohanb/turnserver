cad4f825c494cb5c7ab96176bb93912a00903c04
Rajmohan <rajmohan@mindbricks.com>
2013-08-01 12:49:39 +0530

    minor change for debugging chrome client

--- app/models/ephemeral_credential.rb
+++ app/models/ephemeral_credential.rb
@@ -10,8 +10,8 @@ class EphemeralCredential < ActiveRecord::Base
   end
 
   def uris
-    #["turn:127.0.0.1:3478?transport=udp"]
-    #["turn:50.116.13.218:3478?transport=udp"]
-    ["turn:50.116.13.218:3478"]
+    #["turn:127.0.0.1:3478"]
+    ["turn:50.116.13.218:3478?transport=udp"]
+    #["turn:50.116.13.218:3478"]
   end
 end
