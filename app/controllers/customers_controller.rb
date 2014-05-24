4a213cfb66a2aef210ed0846d7fc141d511aef25
Rajmohan <rajmohan@mindbricks.com>
2014-05-24 23:49:42 +0530

    Changes to display auth token and fix the update for customer model
    This commit adds the following fixes
    
    1) The customer model edit dialog/form has been changed to show the REST
    API authentication token. This field in the form is read only and is
    displayed so that the user can use it for REST API communication.
    
    2) There was a bug where the update action was not being acted upon. So
    the fields in the customer model were not being committed to the
    database. This has been fixed in this commit.

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -11,5 +11,21 @@ class CustomersController < ApplicationController
 
   def update
     @customer = current_customer
+    #if @customer.update_attributes(params[:customer])
+    if @customer.update_attributes!(update_params)
+      # handle a successful update
+      flash[:success] = "Profile updated"
+      redirect_to @customer
+    else
+      # print the errors to the development log
+      Rails.logger.info(@customer.errors.messages.inspect)
+      flash[:error] = "Something wrong? Profile NOT updated"
+      render 'edit'
+    end
   end
+
+  private
+    def update_params
+      params[:customer].slice(:first_name, :last_name)
+    end
 end
