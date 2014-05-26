45957400ccf293e2df6c9cb03a23539a19f7b4c2
Rajmohan <rajmohan@mindbricks.com>
2014-05-27 00:45:04 +0530

    Minor modification to the notice message once the contact is submitted
    Minor modification to the notice message once the contact is submitted.

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -7,7 +7,7 @@ class MessagesController < ApplicationController
     @message = Message.new(params[:message])
     if @message.valid?
       ContactusMailer.new_message(@message).deliver
-      redirect_to root_url, notice: "Message sent! Thank you for contacting us."
+      redirect_to root_url, notice: "Thank you for contacting us. We will get in touch with you shortly"
     else
       flash.now.alert = "Please fill all the fields."
       render "new"
